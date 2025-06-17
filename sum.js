1b1c64438c2001d8ac2d92d3f918e9cce8a91148
farhana sherin c <sherinthanseer@gmail.com>
2025-06-17 12:35:09 +0530

    Initial commit

--- sum.js
+++ sum.js
@@ -1,4 +1,39 @@
-let a=prompt("enter first number:")
-let b=prompt("enter second number:")
-let result=parseInt(a)+parseInt(b)
-console.log(result)
+//  if condition
+const age=18;
+if(age>=18){
+    console.log("you are major");
+}
+// if else
+
+if(age<=18){
+    console.log("you are  minor");
+}
+else{
+    console.log("you are major");
+}
+
+// else if
+const score=95;
+if(score>=90){
+    console.log("Excellent");
+}
+else if(score>=70){
+    console.log("good job");
+}
+else{
+    console.log("keep practicing")
+}
+
+// switch
+ const day="monday"
+ switch(day)
+ {
+    case "monday":
+        console.log("start of the week");
+        break;
+    case "friday":
+        console.log("end of the week");
+        break;   
+    default:
+          console.log("regular day");
+ }
