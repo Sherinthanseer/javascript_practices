fd51138c397bb635d9ebd0ace349322b5f586c47
farhana sherin c <sherinthanseer@gmail.com>
2025-06-17 12:02:07 +0530

    Initial commit

---
+++ op.js
@@ -0,0 +1,20 @@
+
+
+// assign
+var a= 100;
+var b =20;
+
+// addition
+var sum = a+b;
+console.log("sum=",sum)
+
+//subtraction
+var diff = a-b;
+console.log("diffrence=",diff)
+
+// muliplication
+var p= a*b;
+console.log("product=",p)
+
+
+
---
+++ sum.js
@@ -0,0 +1,4 @@
+let a=prompt("enter first number:")
+let b=prompt("enter second number:")
+let result=parseInt(a)+parseInt(b)
+console.log(result)
